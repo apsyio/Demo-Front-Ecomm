af192cc25ab071cd395d1072bf25b167ea5cb457
Hamed <hamed.azadegan@gmail.com>
2022-02-05 12:41:14 +0330

    added splash screen

--- App.tsx
+++ App.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
 
 import AppNavigator from '~/navigation/AppNavigator';
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return <AppNavigator />;
 }
