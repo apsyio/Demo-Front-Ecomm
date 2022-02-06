48773de78494e201e77046f20a2ddd4968d29e80
Hamed <hamed.azadegan@gmail.com>
2022-02-06 10:58:57 +0330

    added flex, bg color

--- src/components/atoms/Container/index.tsx
+++ src/components/atoms/Container/index.tsx
@@ -1,6 +1,12 @@
 import {View} from 'native-base';
 import React from 'react';
 
+import {Colors} from '~/styles';
+
 export default function Container({...props}) {
-  return <View {...props}>{props.children}</View>;
+  return (
+    <View flex={1} background={Colors.WHITE} {...props}>
+      {props.children}
+    </View>
+  );
 }
