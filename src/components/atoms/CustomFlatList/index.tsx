3e685110ef9bc7e053389b7da12a6608a7a12b6a
Hamed <hamed.azadegan@gmail.com>
2022-02-12 19:41:50 +0330

    fix: FlatList prop type

--- src/components/atoms/CustomFlatList/index.tsx
+++ src/components/atoms/CustomFlatList/index.tsx
@@ -1,7 +1,7 @@
 import {FlatList} from 'native-base';
+import {IFlatListProps} from 'native-base/lib/typescript/components/basic/FlatList';
 import React from 'react';
-import {FlatListProps} from 'react-native';
 
-export default function CustomFlatList(props: FlatListProps<any>) {
+export default function CustomFlatList(props: IFlatListProps<any>) {
   return <FlatList onEndReachedThreshold={0.9} {...props} />;
 }
