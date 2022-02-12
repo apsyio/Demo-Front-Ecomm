408f921a4f25f3f268c62bbe5280220bb0a04924
Hamed <hamed.azadegan@gmail.com>
2022-02-12 21:39:43 +0330

    feat: integrate styles screen query

--- src/screens/styles/StylesScreen/index.tsx
+++ src/screens/styles/StylesScreen/index.tsx
@@ -2,21 +2,23 @@ import {FlatList, View} from 'native-base';
 import React from 'react';
 
 import {CustomContainer, ImageCard} from '~/components/atoms';
+import useGetStyles from '~/hooks/styles/useGetStyles';
 import {navigate} from '~/navigation/methods';
 
 export default function StylesScreen() {
+  const {isRefetching, data, fetchNextPage, hasNextPage, refetch} =
+    useGetStyles({});
+
   return (
     <CustomContainer>
       <FlatList
         numColumns={2}
-        data={[
-          {id: 1, title: 'CASUAL', uri: 'https://picsum.photos/200'},
-          {id: 2, title: 'STREET CASUAL', uri: 'https://picsum.photos/200'},
-          {id: 3, title: 'ELEGANT', uri: 'https://picsum.photos/200'},
-          {id: 4, title: 'SPORT CASUAL', uri: 'https://picsum.photos/200'},
-          {id: 5, title: 'EXOTIC', uri: 'https://picsum.photos/200'},
-          {id: 6, title: 'GRUNGE', uri: 'https://picsum.photos/200'},
-        ]}
+        refreshing={isRefetching}
+        onRefresh={refetch}
+        keyExtractor={(item, index) =>
+          item?.id ? item?.id?.toString() : index?.toString()
+        }
+        data={data?.pages}
         renderItem={({item}) => (
           <View style={{width: '50%', marginTop: 10}}>
             <ImageCard
@@ -25,6 +27,11 @@ export default function StylesScreen() {
             />
           </View>
         )}
+        onEndReached={() => {
+          if (hasNextPage) {
+            fetchNextPage();
+          }
+        }}
       />
     </CustomContainer>
   );
