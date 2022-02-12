9f9ed89d52a96ceb09d27a86f7447bcb8f60e525
Hamed <hamed.azadegan@gmail.com>
2022-02-12 21:39:53 +0330

    feat: integrate inspos screen query

--- src/screens/inspo/InspoScreen/index.tsx
+++ src/screens/inspo/InspoScreen/index.tsx
@@ -2,45 +2,38 @@ import {FlatList} from 'native-base';
 import React from 'react';
 
 import {CustomContainer, ImageCard} from '~/components/atoms';
+import useGetInspos from '~/hooks/inspo/useGetInspos';
 import {navigate} from '~/navigation/methods';
 
 export default function InspoScreen() {
+  const {isRefetching, data, fetchNextPage, hasNextPage, refetch} =
+    useGetInspos({});
+
   return (
     <CustomContainer>
       <FlatList
-        numColumns={2}
         columnWrapperStyle={{justifyContent: 'space-around'}}
-        data={[
-          {
-            id: 1,
-            title: 'Isabella Hutton',
-            uri: 'https://picsum.photos/200',
-          },
-          {
-            id: 2,
-            title: 'Isabella Hutton',
-            uri: 'https://picsum.photos/200',
-          },
-          {
-            id: 3,
-            title: 'Isabella Hutton',
-            uri: 'https://picsum.photos/200',
-          },
-          {
-            id: 4,
-            title: 'Isabella Hutton',
-            uri: 'https://picsum.photos/200',
-          },
-        ]}
+        numColumns={2}
+        refreshing={isRefetching}
+        onRefresh={refetch}
+        keyExtractor={(item, index) =>
+          item?.id ? item?.id?.toString() : index?.toString()
+        }
+        data={data?.pages}
         renderItem={({item}) => (
           <ImageCard
             containerStyle={{marginTop: 16}}
             hasBorder
             {...item}
-            bottomTitle={item.title}
+            bottomTitle={item.fullName}
             onPress={() => navigate('MyProfile', {id: item.id})}
           />
         )}
+        onEndReached={() => {
+          if (hasNextPage) {
+            fetchNextPage();
+          }
+        }}
       />
     </CustomContainer>
   );
