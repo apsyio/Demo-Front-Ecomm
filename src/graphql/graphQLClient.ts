675aaa1c2b80a1d16503576bef7e42bfc031b8c0
Hamed <hamed.azadegan@gmail.com>
2022-02-12 14:52:27 +0330

    feat: add react-native-config

--- src/graphql/graphQLClient.ts
+++ src/graphql/graphQLClient.ts
@@ -1,7 +1,6 @@
 import {GraphQLClient} from 'graphql-request';
+import Config from 'react-native-config';
 
-const graphQLClient = new GraphQLClient(
-  'https://aps-cueoncurves-dev.azurewebsites.net/graphql/',
-);
+const graphQLClient = new GraphQLClient(Config.API_URL);
 
 export default graphQLClient;
