1bcd981629004ec5619f73c87fa5e8e9c7e96b26
Hamed <hamed.azadegan@gmail.com>
2022-02-13 16:34:59 +0330

    feat: store active tab in store

---
+++ src/store/createActiveTabSlice.ts
@@ -0,0 +1,15 @@
+import {StateCreator} from 'zustand';
+
+type ActiveTabType = 'Home' | 'Brands' | 'Feed' | 'Styles' | 'Inspo';
+
+export type ActiveTabState = {
+  activeTab: ActiveTabType;
+  setActiveTab: (activeTab: ActiveTabType) => void;
+};
+
+export const createActiveTabSlice: StateCreator<ActiveTabState> = set => ({
+  activeTab: 'Home',
+  setActiveTab: (activeTab: ActiveTabType) => set({activeTab}),
+});
+
+export default createActiveTabSlice;
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,10 +2,11 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import create, {GetState, SetState, StoreApi} from 'zustand';
 import {persist} from 'zustand/middleware';
 
+import createActiveTabSlice, {ActiveTabState} from './createActiveTabSlice';
 import {AuthState, createAuthSlice} from './createAuthSlice';
 import {createOnboardingSlice, OnboardingState} from './createOnboardingSlice';
 
-interface IStore extends AuthState, OnboardingState {}
+interface IStore extends AuthState, OnboardingState, ActiveTabState {}
 
 /**
  * Make sure to enforce type for each slice
@@ -23,6 +24,11 @@ export const useStore = create<IStore>(
         set as unknown as SetState<OnboardingState>,
         get as GetState<OnboardingState>,
         api as unknown as StoreApi<OnboardingState>,
+      ),
+      ...createActiveTabSlice(
+        set as unknown as SetState<ActiveTabState>,
+        get as GetState<ActiveTabState>,
+        api as unknown as StoreApi<ActiveTabState>,
       ),
     }),
     {
